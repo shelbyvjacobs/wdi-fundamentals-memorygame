67a2ea7a369aaeccddf146583b3308721aa222b7
Shelby Jacobs <shelby@Shelbys-MacBook-Air.local>
2019-09-18 15:17:31 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,22 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if(cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again")
+
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again")
+		}
 	}
 }
+
+function flipCard(cardID){
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
