873dde1c1bb7fa7a6f727eabdabb091213744d98
Shelby Jacobs <shelby@Shelbys-MacBook-Air.local>
2019-09-24 10:05:58 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,23 +22,33 @@ const cards = [
 ];
 const cardsInPlay = [];
 
-function checkForMatch(){
-	if(cardsInPlay.length === 2){
-		if (cardsInPlay[0] === cardsInPlay[1]){
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again")
-		}
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
 	}
-}
-
-function flipCard(cardID){
-	console.log("User flipped " + cards[cardID].rank);
-	cardsInPlay.push(cards[cardID].rank);
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
-	checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+};
+
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i ++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
